0990df9e0f2e6bebd79b4794ac2aa1528115aa38
Erica1912 <eri.ruiz@hotmail.com.ar>
2021-03-31 15:29:12 -0500

    queryselectorall

--- src/queryselector.js
+++ src/queryselector.js
@@ -1,4 +1,4 @@
-const querySelector = () => {
+const FuncQuerySelector = () => {
     document.querySelector("a.classDos").style.backgroundColor = "red";
     document.querySelector("a.classDos").innerHTML = "Hello World!";
     document.querySelector("a.classDos").href ="www.aqui.com"
--- src/queryselectorall.js
+++ src/queryselectorall.js
@@ -1,3 +1,13 @@
-const querySelectorAll = () => {
-    console.log("Hola aquí")
-}
+const FuncQuerySelectorAll = () => {
+  const elementArray = document.querySelectorAll(
+    "a.classTres"
+  ); /*Nos devuelve el número de elementos*/
+
+  console.log("aqui");
+  for (let i = 0; i < elementArray.length; i++) {
+      /*Agregamos un texto diferente ademas cambiamos el fondo y el color del texto*/
+    elementArray[i].textContent = "cambio";
+    elementArray[i].style.color = "white";
+    elementArray[i].style.backgroundColor = "red";
+  }
+};
